a5f0849e584fb18febb95d00aa04104471c93400
Erik vz <erik@netninja.nl>
2020-09-01 14:19:30 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,8 +1,11 @@
 <?php
 require 'RedBeanPHP5_5/rb.php';
-R::setup( 'mysql:host=localhost;dbname=WeddingWall',
-'root', 'root' ); //for both mysql or mariaDB
-$post = R::dispense( 'post' );
+R::setup(
+    'mysql:host=localhost;dbname=WeddingWall',
+    'root',
+    'root'
+); //for both mysql or mariaDB
+$post = R::dispense('post');
 $post->title = '123hoedjevanpapier';
-$id = R::store( $post );
+$id = R::store($post);
 ?>
