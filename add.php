811a052847e68fcd7856089f5ab1f8209bcb1558
Erik vz <erik@netninja.nl>
2020-09-01 16:16:25 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,11 +1,9 @@
-
 <?php
 include 'inc.php';
-
-
-    //test post
-$post = R::dispense('post');
-$post->title = $_POST["message"];
-$post->author = $_POST["author"];
-$post->created = date("Y-m-d H:i:s");
-$id = R::store($post);
+    //post message
+        $post = R::dispense('post');
+        $post->title = $_POST["message"];
+        $post->author = $_POST["author"];
+        $post->created = date("Y-m-d H:i:s");
+        $id = R::store($post);
+?>
