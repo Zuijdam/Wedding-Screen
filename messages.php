c1d36d56463b892ff9fd2290116cd646775ab0bb
Erik vz <erik@netninja.nl>
2020-09-08 22:10:00 +0200

    volgens mij gaat dit ook goed

--- messages.php
+++ messages.php
@@ -1,18 +1,13 @@
 
-
 <?php
 include 'inc.php';
-
-//echo '<b>Laatste update:</b><br>';
 $post2 = R::findLast('post');
-
 
 echo '<p class="lead"><strong>'.$post2->author.' </strong>schrijft zojuist: </p>';
 echo '<h1 class="display-4">'.$post2->title.'</h1>';
 echo '<hr>';
 //echo '<p class="lead"><footer class="blockquote-footer">'.$post2->author.' (<cite title="Source Title">'.toNiceTimeElapsed($post2->created).')</cite></footer> </p>';
 echo '<p class="lead"><footer class="blockquote-footer"><cite title="Source Title">'.toNiceTimeElapsed($post2->created).'</cite></footer> </p>';
-
 echo '<div class="hidden">'.$post2->id.'</div>';
 
 R::close();
