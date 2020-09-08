f75e54077292047922da2ee70d3c55aff19efb27
Joris <jorisreijbroek@gmail.com>
2020-09-08 20:38:55 +0200

    Deleten van berichten

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+include 'inc.php';
+if($_GET["key"] == $deleteKey){
+    $bericht = R::load('post', $_GET["id"]);
+    R::trash($bericht);
+    echo "bericht met id ".$_GET["id"]." verwijderd";
+}
+?>
