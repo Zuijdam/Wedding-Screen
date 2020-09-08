d72d1e983d9496d81bcca5e2cb4e3ff8e936f96e
Joris <jorisreijbroek@gmail.com>
2020-09-08 20:20:25 +0200

    message verplicht

--- add.php
+++ add.php
@@ -1,11 +1,13 @@
 <?php
 include 'inc.php';
     //post message
-        $post = R::dispense('post');
-        $post->title = $_POST["message"];
-        $post->author = $_POST["author"];
-        $post->created = date("Y-m-d H:i:s");
-        $id = R::store($post);
+if($_POST["message"]){
+    $post = R::dispense('post');
+    $post->title = $_POST["message"];
+    $post->author = $_POST["author"];
+    $post->created = date("Y-m-d H:i:s");
+    $id = R::store($post);
+
 
 ?>
 
@@ -40,3 +42,9 @@ include 'inc.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
+<?php
+}
+else{
+  echo "Geen bericht ingevuld <br> <a href='form.php'>ga terug</a>";
+}
+?>
