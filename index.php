bfb0153d9819e9b74abc9f5e1bd22f16224a1bde
Erik vz <erik@netninja.nl>
2020-09-08 22:15:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,50 +6,50 @@ include 'inc.php';
 <!doctype html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="includes/bootstrap.min.css">
-        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-        -->
-        <link rel="stylesheet" href="includes/style.css">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="includes/bootstrap.min.css">
+		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+		-->
+		<link rel="stylesheet" href="includes/style.css">
 
-        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-        <title>WeddingWall</title>
-    </head>
+		<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+		<title>WeddingWall</title>
+	</head>
 
-    <body>
-       
-        
-            <div class="container d-flex h-100">
-                <div class="row align-self-center w-100">
-                            <span id="home">
-                                <script>
-                                $(document).ready(function(){
-                                setInterval(function(){
-                                $("#home").load('messages.php')
-                                }, 3000);
-                                });
-                                </script> 
-                            </span>
-                </div>
-                 
-                                    
-            <!-- QR dingetje hier -->
+	<body>
 
-            <div class="fixed-bottom">
-                <div class="float-right">
-                Scan de QR Code of ga naar <a href="<?php echo urlencode($QRLink) ?>"><?php echo $QRLink ?></a> 
-                    <img src='https://chart.googleapis.com/chart?cht=qr&chl=<?php echo urlencode($QRLink) ?>&chs=150x150'>
-                </div>
-                <br>
-                
-            </div>
 
-            <!--hier-->
-        </div>  
+			<div class="container d-flex h-100">
+				<div class="row align-self-center w-100">
+							<span id="home">
+								<script>
+								$(document).ready(function(){
+								setInterval(function(){
+								$("#home").load('messages.php')
+								}, 3000);
+								});
+								</script>
+							</span>
+				</div>
 
-        
-    </body>
+
+			<!-- QR dingetje hier -->
+
+			<div class="fixed-bottom">
+				<div class="float-right">
+				Scan de QR Code of ga naar <a href="<?php echo $QRLink ?>"><?php echo $QRLink ?></a>
+					<img src='https://chart.googleapis.com/chart?cht=qr&chl=<?php echo urlencode($QRLink) ?>&chs=150x150'>
+				</div>
+				<br>
+
+			</div>
+
+			<!--hier-->
+		</div>
+
+
+	</body>
 </html>
