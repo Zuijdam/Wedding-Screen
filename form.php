ec8e0491effacdc76d19118f049b7db2bd71a950
Joris <jorisreijbroek@gmail.com>
2020-09-10 09:26:45 +0200

    verplicht label toegevoegd

--- form.php
+++ form.php
@@ -24,10 +24,12 @@
         <div class="form-group">   
           <label>Je naam</label>
           <input type="text" class="form-control" name="author">
+          <small class="form-text text-muted">verplicht</small>
         </div>
         <div class="form-group"> 
           <label>Je bericht</label>
           <textarea rows="4" class="form-control" name="message" maxlength = "200"></textarea>
+          <small class="form-text text-muted">verplicht</small>
         </div>
           <button class="btn btn-dark" type="submit">versturen</button>
         </div>
