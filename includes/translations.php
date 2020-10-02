e3f10ba866cbbdde9d24aae19498c9b8bfcce549
Erik vz <erik@netninja.nl>
2020-10-02 14:51:14 +0200

    translation file iets anders ingericht

--- includes/translations.php
+++ includes/translations.php
@@ -2,34 +2,32 @@
 
 //Main
 define("translation_title", "WeddingWall 0.1 - Geensnor.nl");
-define ("translation_deletemessageid", "bericht verwijdert met id:");
-define ("translation_delete", "delete");
-define ("translation_messagesaved", "Bedankt! Je bericht is opgeslagen en over enkele ogenblikken te zien op het scherm!");
-define ("translation_alreadyexist", "bestaat al");
-define ("translation_nomessage", "geen bericht ingevuld");
-define ("translation_back", "terug");
-define ("translation_newmessage", "nieuw bericht");
 
 //WeddingWall translations
 define("translation_wrote", "schreef");
 define("translation_dragdiv", "Drag me!");
 
-//Form Translations
+//Add messages translations
 define ("translation_formtitle", "Insert title here");
 define ("translation_formdescription", "Insert description here");
 define ("translation_formlabelname", "name");
 define ("translation_formlabelmessage", "Insert message here");
 define ("translation_formlabelmandatory", "label mandatory");
 define ("translation_formlabelsendbutton", "label send");
-
+define ("translation_messagesaved", "Bedankt! Je bericht is opgeslagen en over enkele ogenblikken te zien op het scherm!");
+define ("translation_alreadyexist", "bestaat al");
+define ("translation_nomessage", "geen bericht ingevuld");
+define ("translation_back", "terug");
+define ("translation_newmessage", "nieuw bericht");
 
 //Admin translations
+define ("translation_deletemessageid", "bericht verwijdert met id:");
+define ("translation_delete", "delete");
 
-//Function translations
+//General function translations
 define("translation_minutes_ago", "minuten geleden");
 define("translation_hours_ago", "uren geleden");
 define("translation_just", "zojuist");
 define("translation_days_ago", "dagen geleden");
-
 
 ?>
