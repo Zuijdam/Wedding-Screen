f51532de4fac8a44595e3c1cac4bc3475695ccc9
ErikvZ <erik@netninja.nl>
2020-10-02 11:22:28 +0200

    Revert "Typo"
    This reverts commit 276bea05801ba661392302610eb263832a04c579.

--- includes/functions.php
+++ includes/functions.php
@@ -6,7 +6,7 @@ function toNiceTimeElapsed($date){
 	if(date("Ymd") == date("Ymd", strtotime($date))){
 	    if($sec < 60) {
 	      /* if less than a minute, return seconds */
-	      return translation_just;
+	      return just;
 	    }
 	    else if($sec < 60*60) {
 		  /* if less than an hour, return minutes */
